da2f6dfd8003e0d60e8cd910500ed760763f9aa8
yehchunting <2017yehchunting@gmail.com>
2025-08-05 03:22:40 +0800

    d

--- 4.characterArraysAndPointers/stringAsFunctionArgument.c
+++ 4.characterArraysAndPointers/stringAsFunctionArgument.c
@@ -1,20 +1,19 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void print(char *C){
-    printf("%s\n",C);
+void print(char *C) {       // 以指標接收字串
+    printf("%s\n", C);      // 印出整個字串
 
-    int i=0;
-
-    while(C[i]!= '\0'){
-        printf("%c",*(C+i)); //same as C[i]
+    int i = 0;
+    while (C[i] != '\0') {  // 逐字元列印
+        printf("%c", *(C+i)); // same as C[i]
         i++;
     }
     printf("\n");
 }
 
-int main(){
-    char C[6]="Hello";
-    print(C); 
+int main() {
+    char C[6] = "Hello";
+    print(C);               // 傳入字元陣列 → 自動退化為指標
     return 0;
 }
